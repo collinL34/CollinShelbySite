2b4eabe0a3e9d095850247c5ca528e4240ca84ac
collinl34 <collinlshelby@gmail.com>
2017-03-20 16:24:21 -0700

    ngMap module add to application to fix googleMaps bug map functioning correctly styling still need

--- public/js/controllers/ContactCtrl.js
+++ public/js/controllers/ContactCtrl.js
@@ -1,3 +1,9 @@
 angular.module('ContactCtrl', [])
-    .controller('ContactCtrl', function($scope) {
+    .controller('ContactCtrl', function($scope, NgMap) {
+        $scope.googleMapsUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDaWri2T7LD6xTgFZNwjLZsQZp0WPa2W2U';
+        NgMap.getMap().then(function(map) {
+            console.log(map.getCenter());
+            console.log('markers', map.markers);
+            console.log('shapes', map.shapes);
+        });
     })
