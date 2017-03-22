bd9ad04ebf6688727751dacb9221b3960504fe38
collinl34 <collinlshelby@gmail.com>
2017-03-22 14:38:23 -0700

    :construction: heroku testing with google maps api still in progress

--- public/js/controllers/ContactCtrl.js
+++ public/js/controllers/ContactCtrl.js
@@ -2,14 +2,14 @@ angular.module('ContactCtrl', ['ngMap', 'Angular.config'])
     .controller('ContactCtrl', function($scope, NgMap, GOOGLE) {
         $scope.googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=" + GOOGLE;
 
-        // NgMap.getMap().then(function(map) {
-        //     console.log(map.getCenter());
-        //     console.log('markers', map.markers);
-        //     console.log('shapes', map.shapes);
-        // });
-        NgMap.getMap('gmap', { timeout: 200000 }).then(function(map) {
-            console.log('map initialized');
-        }).catch(function(map) {
-            console.error('map error: ', map);
+        NgMap.getMap().then(function(map) {
+            console.log(map.getCenter());
+            console.log('markers', map.markers);
+            console.log('shapes', map.shapes);
         });
+        // NgMap.getMap('gmap', { timeout: 200000 }).then(function(map) {
+        //     console.log('map initialized');
+        // }).catch(function(map) {
+        //     console.error('map error: ', map);
+        // });
     })
