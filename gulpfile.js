b77349a90fc3a92738594ec908ebdf8f614026e3
collinl34 <collinlshelby@gmail.com>
2017-03-22 14:08:11 -0700

    heroku pull

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const gulpNgConfig = require('gulp-ng-config');
+
 gulp.task('test', function() {
     gulp.src('AppConfig.json')
         .pipe(gulpNgConfig('Angular.config'))
