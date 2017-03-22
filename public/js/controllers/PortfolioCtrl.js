4d41e8bde594253295ef3e6b3c3caf2d1d685e32
collinl34 <collinlshelby@gmail.com>
2017-03-22 12:02:48 -0700

    :sparles: ui.router links to projects in porftolio page functioning add methods to portfolioCtrl when clicked redirect to website

--- public/js/controllers/PortfolioCtrl.js
+++ public/js/controllers/PortfolioCtrl.js
@@ -1,4 +1,10 @@
 angular.module('PortfolioCtrl', [])
     .controller('PortfolioCtrl', function($scope) {
+        $scope.Autism = function() {
+            window.location = 'http://autism-talks.herokuapp.com/';
+        };
 
+        $scope.bearHugs = function() {
+            window.location = 'http://bear-hug.herokuapp.com/';
+        };
     })
