37814b2b95d7ace3dc16e2a0e4d71a49e6edeaf3
collinl34 <collinlshelby@gmail.com>
2017-03-22 23:36:15 -0700

    functions add to social links in navbar

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,4 +1,16 @@
 angular.module('HomeCtrl', [])
     .controller('HomeCtrl', function($scope) {
         $scope.greeting = 'hey there you';
+
+        $scope.facebook = function() {
+            window.location = 'https://www.facebook.com/collin.shelby';
+        };
+
+        $scope.github = function() {
+          window.location = 'https://github.com/collinL34/';
+        };
+
+        $scope.linkedin = function() {
+          window.location = 'https://www.linkedin.com/in/collin-shelby/';
+        }
     })
