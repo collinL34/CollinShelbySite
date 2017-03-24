91fdae80ad616acd8374d6af1a4b2eb3358d7dc3
collinl34 <collinlshelby@gmail.com>
2017-03-23 19:07:55 -0700

    nodemailer integrate into application, bug with nodemailer sends email correctly but always has my email as the sender

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,41 @@
+const nodemailer = require('nodemailer');
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'collinlshelby@gmail.com',
+        pass: 'alpacin12'
+    }
+});
+
+
 module.exports = function(app) {
 
     app.get('*', function(req, res) {
-        res.sendfile('./public/index.html'); // load our public/index.html file
+        res.sendfile('./public/index.html');
     });
 
-    app.get('/contact', function(req,res) {
-      res.sendfile('../public/views/contact.html');
+    app.get('/contact', function(req, res) {
+        res.sendfile('../public/views/contact.html');
     });
 
-    app.get('/portfolio', function(req,res) {
-      res.sendfile('../public/views/portfolio.html');
+    app.get('/portfolio', function(req, res) {
+        res.sendfile('../public/views/portfolio.html');
+    });
+
+    app.post('/contact-email', function(req, res) {
+        let mailOptions = {
+            to: 'collinlshelby@gmail.com',
+            from: 'Fred Foo 👻 <rawnchychero@comcast.net>',
+            sender: req.body.sender,
+            subject: req.body.subject,
+            text: req.body.message
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+        });
     });
 };
--- public/js/controllers/ContactCtrl.js
+++ public/js/controllers/ContactCtrl.js
@@ -1,12 +1,18 @@
 angular.module('ContactCtrl', ['Angular.config'])
-    .controller('ContactCtrl', function($scope) {
-        $scope.email = {
-            subject: undefined,
-            sender: undefined,
-            message: undefined
-        };
+    .controller('ContactCtrl', function($scope, $http) {
+        $scope.email = {};
 
         $scope.sendEmail = function() {
-            console.log($scope.message, $scope.subject, $scope.sender);
-        }
+            $scope.email = { subject: $scope.subject, sender: $scope.sender, message: $scope.message };
+
+            console.log($scope.email);
+            $http.post('/contact-email', $scope.email)
+                .then(function(data) {
+                        console.log(data, 'success');
+                        $scope.email = {};
+                    },
+                    function(response) {
+                        console.log(response, 'error');
+                    });
+        };
     })
