9af183740d58ceb110e81c3cfbc20950a4d372be
collinl34 <collinlshelby@gmail.com>
2017-03-25 15:59:28 -0700

    form inputs remove for contact form

--- public/js/controllers/ContactCtrl.js
+++ public/js/controllers/ContactCtrl.js
@@ -5,14 +5,16 @@ angular.module('ContactCtrl', ['Angular.config'])
         $scope.sendEmail = function() {
             $scope.email = { name: $scope.name, subject: $scope.subject, sender: $scope.sender, message: $scope.message };
 
-            console.log($scope.email);
             $http.post('/contact-email', $scope.email)
                 .then(function(data) {
                         console.log(data, 'success');
-                        $scope.email = {};
                     },
                     function(response) {
                         console.log(response, 'error');
                     });
+            $scope.name = '';
+            $scope.sender = '';
+            $scope.subject = '';
+            $scope.message = '';
         };
     })
